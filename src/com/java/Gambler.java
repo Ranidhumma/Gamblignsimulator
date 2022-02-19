13b65bd8751d94111d25abc70f9e0c9934462031
Ranidhumma <ranidhumma8@gmail.com>
2022-02-19 12:16:34 +0530

    Gambler resign of he won or loose by 50 %

--- src/com/java/Gambler.java
+++ src/com/java/Gambler.java
@@ -1,13 +1,59 @@
 package com.java;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Gambler {
+    public static final int BET = 1;
+	public static final int Stake_Day = 100 ;
+	public static final int WIN = 1;
+    public static int stake = 0;
 
+	public static Scanner scanner = new Scanner(System.in);
+	public static void Gambler(){
+		
+		//Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a stake value");
+		int stake = scanner.nextInt();
+		System.out.println("Enter a bet Value");
+		int bet = scanner.nextInt();
+	}
 	
+	public static void wincheck() {
+		//Random random = new Random();
+		//int bet = random.nextInt(2);
+		double bet= Math.floor(Math.random() * 10)% 2;
+		if (bet == WIN) 
+		{
+			stake++ ;
+		}
+		else {
+			stake--; 
+			}
+      
+		System.out.println("stake= " +stake);
+	}
+		public static void CheckResignDay(){
+			int totalstake = 0;
+			
+			while (stake!= 50 && stake != -50)
+			{
+				wincheck();
+			}
+		  totalstake = stake + Stake_Day ;
+		
+			System.out.println(+totalstake);
+		
+	scanner.close();
+}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
-	System.out.println("Welcome to gamblerNew");	
+		System.out.println("Welcome");
+	
+		CheckResignDay();
 	}
-
+	
 }
+
+
