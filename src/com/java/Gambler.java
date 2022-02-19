d6fb41c6a54effa7412c956889dadf5704784622
Ranidhumma <ranidhumma8@gmail.com>
2022-02-19 08:54:20 +0530

    Welcome

---
+++ src/com/java/Gambler.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Gambler {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+	System.out.println("Welcome to gamblerNew");	
+	}
+
+}
