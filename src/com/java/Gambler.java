5ac3341cdee24bfdf9a2054bdd48bdacb2242da6
Ranidhumma <ranidhumma8@gmail.com>
2022-02-19 08:58:54 +0530

    gambler_ini

--- src/com/java/Gambler.java
+++ src/com/java/Gambler.java
@@ -3,11 +3,20 @@ package com.java;
 import java.util.Scanner;
 
 public class Gambler {
-
+	public static void Gambler() {
+		int initial_Stake = 100 ;
+		int bet = 1;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a Stake value");
+		int Stake = scanner.nextInt();
+		System.out.println("Enter a bet money");
+		int Bet = scanner.nextInt();
+	}
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
-	System.out.println("Welcome to gamblerNew");	
+		
 	}
-
+	
 }
